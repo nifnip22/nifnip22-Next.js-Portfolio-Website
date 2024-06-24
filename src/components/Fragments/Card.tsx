4f03d2d17ad4627fc0c7fffc26978135aafa6a5c
nifnip22 <proxigaming742@gmail.com>
2024-06-24 19:13:00 +0800

    Card Outline when Hover

--- src/components/Fragments/Card.tsx
+++ src/components/Fragments/Card.tsx
@@ -1,8 +1,8 @@
-export default function Card({ title, subtitle }: { title: string, subtitle: string | React.ReactNode }) {
+export default function Card({ title, subtitle }: { title: string; subtitle: string | React.ReactNode }) {
 	return (
 		<>
 			{/* Card */}
-			<div className='w-2/5 h-4/5 bg-gray-100/30 rounded-[40px] p-5'>
+			<div className='w-2/5 h-4/5 bg-gray-100/30 rounded-[40px] p-5 hover:outline hover:outline-gray-100 hover:outline-4 hover:outline-offset-8 duration-300'>
 				<div className='h-full flex flex-col justify-between'>
 					<h1 className='text-4xl text-gray-100 font-semibold leading-relaxed'>{title}</h1>
 					<div className='text-2xl text-gray-100 leading-relaxed'>{subtitle}</div>
