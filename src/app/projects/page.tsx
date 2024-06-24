cae33ec35ede24596a9be3e6b473aec89dceecc9
nifnip22 <proxigaming742@gmail.com>
2024-06-24 19:06:43 +0800

    Add Interactive Hover on Contacts Navbar and Made Projects Route

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,16 @@
+import MainLayout from '@/components/Layouts/MainLayout';
+
+export default function Projects() {
+	return (
+		<>
+			<MainLayout>
+				<div className='h-4/5 flex flex-col justify-start gap-y-20'>
+					<h1 className='w-[590px] h-[160px] text-8xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-slate-500 to-gray-100'>My Projects</h1>
+					<div className='h-full flex flex-nowrap gap-x-20'>
+
+                    </div>
+				</div>
+			</MainLayout>
+		</>
+	);
+}
--- src/components/Fragments/Navbar.tsx
+++ src/components/Fragments/Navbar.tsx
@@ -6,23 +6,39 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import Link from 'next/link';
 
 export default function Navbar() {
-    return (
-        <>
-            {/* Navbar */}
-				<div className='flex justify-between items-center'>
-					<div className='flex items-center gap-x-20 text-lg text-gray-100'>
-						<Link href={"/"}><h3>Home</h3></Link>
-						<Link href={"/about"}><h3>About</h3></Link>
+	return (
+		<>
+			{/* Navbar */}
+			<div className='flex justify-between items-center'>
+				<div className='flex items-center gap-x-20 text-lg text-gray-100'>
+					<Link href={'/'}>
+						<h3>Home</h3>
+					</Link>
+					<Link href={'/about'}>
+						<h3>About</h3>
+					</Link>
+					<Link href={'/projects'}>
 						<h3>Projects</h3>
-					</div>
-					<div className='flex items-center gap-x-12'>
-						<GitHubIcon className='text-gray-100 text-4xl' />
-						<LinkedInIcon className='text-gray-100 text-4xl' />
-						<FacebookIcon className='text-gray-100 text-4xl' />
-						<InstagramIcon className='text-gray-100 text-4xl' />
-						<XIcon className='text-gray-100 text-4xl' />
-					</div>
+					</Link>
 				</div>
-        </>
-    )
+				<div className='flex items-center gap-x-12'>
+					<a href='https://github.com/nifnip22' target='_blank'>
+						<GitHubIcon className='text-gray-100 opacity-60 hover:opacity-100 hover:scale-125 transition duration-300 text-4xl' />
+					</a>
+					<a href='https://www.linkedin.com/in/hanif-ahmad-18a879303/?trk=opento_sprofile_details' target='_blank'>
+						<LinkedInIcon className='text-gray-100 opacity-60 hover:opacity-100 hover:scale-125 transition duration-300 text-4xl' />
+					</a>
+					<a href='https://www.facebook.com/profile.php?id=100089012786583' target='_blank'>
+						<FacebookIcon className='text-gray-100 opacity-60 hover:opacity-100 hover:scale-125 transition duration-300 text-4xl' />
+					</a>
+					<a href='https://www.instagram.com/hnif.am22/' target='_blank'>
+						<InstagramIcon className='text-gray-100 opacity-60 hover:opacity-100 hover:scale-125 transition duration-300 text-4xl' />
+					</a>
+					<a href='https://x.com/nifnip22' target='_blank'>
+						<XIcon className='text-gray-100 opacity-60 hover:opacity-100 hover:scale-125 transition duration-300 text-4xl' />
+					</a>
+				</div>
+			</div>
+		</>
+	);
 }
