6a171447d535298350ca52d5b25884178f696bc0
nifnip22 <proxigaming742@gmail.com>
2024-06-26 18:50:34 +0800

    Project Framer Motion

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,27 @@
+'use client';
 import ProjectCard from '@/components/Fragments/ProjectCard';
 import MainLayout from '@/components/Layouts/MainLayout';
-import Image from 'next/image';
+import { motion, cubicBezier } from 'framer-motion';
 
 export default function Projects() {
 	return (
 		<>
 			<MainLayout>
 				<div className='h-4/5 flex flex-col justify-start gap-y-20'>
-					<h1 className='2xl:w-[590px] h-[160px] text-7xl sm:text-8xl text-center 2xl:text-start font-bold bg-gradient-to-r text-transparent bg-clip-text from-slate-500 to-gray-100'>My Projects</h1>
-					<div className='h-full flex flex-col 2xl:flex-row gap-x-20 gap-y-20'>
+					<motion.h1
+						initial={{ opacity: 0, y: 40 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.6, ease: cubicBezier(0.37, 0.01, 0.25, 0.95) }}
+						className='2xl:w-[590px] h-[160px] text-7xl sm:text-8xl text-center 2xl:text-start font-bold bg-gradient-to-r text-transparent bg-clip-text from-slate-500 to-gray-100'>
+						My Projects
+					</motion.h1>
+					<motion.div
+						initial={{ opacity: 0, y: 40 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.6, ease: cubicBezier(0.37, 0.01, 0.25, 0.95), delay: 0.3 }}
+						className='h-full flex flex-col 2xl:flex-row gap-x-20 gap-y-20'>
 						<ProjectCard />
-					</div>
+					</motion.div>
 				</div>
 			</MainLayout>
 		</>
