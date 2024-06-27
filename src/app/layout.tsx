893233b3a4d1fef57f3e5283295c2e4baa721a95
nifnip22 <proxigaming742@gmail.com>
2024-06-27 21:04:08 +0800

    Try Metadata Using openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,26 @@ import './styles.css';
 import { Raleway } from 'next/font/google';
 
 export const metadata = {
-  title: 'Hanif Ahmad (nifnip22) | Portfolio Website',
-  description: "Hi, My Name is Hanif Ahmad and Welcome to nifnip22’s First Portfolio Website! I'am a Junior Software Developer Who Has Expertise in Web Developer, Nice to Meet You!",
-  keywords: 'Hanif Ahmad, nifnip22, Web Developer, Portfolio, Software Engineer',
-  author: 'Hanif Ahmad (nifnip22)',
-  url: 'https://nifnip22-portfolio.vercel.app/',
-  image: 'https://nifnip22-portfolio.vercel.app/profile.jpg',
-  charset: 'UTF-8',
-  language: 'en-US',
-  robots: 'index, follow',
-  ogTitle: 'Hanif Ahmad (nifnip22) | Portfolio Website',
-  ogDescription: "Hi, My Name is Hanif Ahmad and Welcome to nifnip22’s First Portfolio Website! I'am a Junior Software Developer Who Has Expertise in Web Developer, Nice to Meet You!",
-  ogImage: 'https://nifnip22-portfolio.vercel.app/profile.jpg',
-  ogUrl: 'https://nifnip22-portfolio.vercel.app/',
-  ogType: 'website',
-  twitterCard: 'summary_large_image',
-  twitterUrl: 'https://nifnip22-portfolio.vercel.app/',
-  twitterTitle: 'Hanif Ahmad (nifnip22) | Portfolio Website',
-  twitterDescription: 'Hi, My Name is Hanif Ahmad and Welcome to nifnip22’s First Portfolio Website! I’am a Junior Software Developer Who Has Expertise in Web Developer, Nice to Meet You!',
-  twitterImage: 'https://nifnip22-portfolio.vercel.app/profile.jpg',
-  twitterSite: '@nifnip22',
+  openGraph: {
+    title: 'Hanif Ahmad (nifnip22) | Portfolio Website',
+    description: "Hi, My Name is Hanif Ahmad and Welcome to nifnip22’s First Portfolio Website! I'am a Junior Software Developer Who Has Expertise in Web Developer, Nice to Meet You!",
+    generator: 'Next.js',
+    keywords: ['Hanif Ahmad, nifnip22, Web Developer, Portfolio, Software Engineer'],
+    author: {name: 'Hanif Ahmad', url: 'https://github.com/nifnip22'},
+    creator: 'Hanif Ahmad',
+    url: 'https://nifnip22-portfolio.vercel.app/',
+    image: {url: 'https://nifnip22-portfolio.vercel.app/profile.jpg'},
+    charset: 'UTF-8',
+    locale: 'en-US',
+    type: 'website',
+    robots: 'index, follow',
+    twitterCard: 'summary_large_image',
+    twitterUrl: 'https://nifnip22-portfolio.vercel.app/',
+    twitterTitle: 'Hanif Ahmad (nifnip22) | Portfolio Website',
+    twitterDescription: 'Hi, My Name is Hanif Ahmad and Welcome to nifnip22’s First Portfolio Website! I’am a Junior Software Developer Who Has Expertise in Web Developer, Nice to Meet You!',
+    twitterImage: {url: 'https://nifnip22-portfolio.vercel.app/profile.jpg'},
+    twitterSite: '@nifnip22',
+  }
 };
 
 export const viewport: Viewport = {
